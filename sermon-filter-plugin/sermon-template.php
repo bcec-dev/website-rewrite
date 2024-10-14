0bfb3650c52c4e80d33a36108fa743de7f98ee3a
waiho.chan <waiho.chan@hcl.com>
2024-10-14 01:32:14 -0400

    update template for display sermon post list

--- sermon-filter-plugin/sermon-template.php
+++ sermon-filter-plugin/sermon-template.php
@@ -9,23 +9,18 @@
     $speakers = get_the_terms(get_the_ID(), 'speaker');
     $speaker_names = $speakers && !is_wp_error($speakers) ? wp_list_pluck($speakers, 'name') : [];
     
-    $show_theme_row = true;
-    $show_date_scripture_in_one_row = true;
     $show_location = true;
     // Set the $title variable based on the filter
     $title = get_the_title(); // Default to the sermon title if no filter is set
-    if ($theme) {
-        $title = $theme;
-        $show_theme_row = false;
+    if ($sermon_date) {
+        $title = $sermon_date;
     }
 
     if (isset($current_taxonomy) && !empty($current_taxonomy)) {
-        $term = get_term_by('slug', $current_taxonomy, $current_filter);
-        if ($term && !is_wp_error($term)) {
-            $title = $term->name . ' - ' . $theme;
-            $show_theme_row = false;
-            // $show_date_scripture_in_one_row = true;
-            $show_location = true;
+        if ($current_filter === 'speaker') {
+            $title = implode(', ', $speaker_names);
+        } else if ( $current_filter === 'scripture') {
+            $title = $verses;
         }
     }
     // Get the categories for the current post
@@ -58,19 +53,16 @@
          <div class="sermon-title">
             <a href="<?php the_permalink(); ?>"><?php echo esc_html($title); ?></a>
         </div>
-        <?php if ($theme && $show_theme_row) : ?>
+        <?php if ($theme) : ?>
             <div class="sermon-theme"><?php echo esc_html($theme); ?></div>
         <?php endif; ?>
-        <?php if ($verses && !$show_date_scripture_in_one_row) : ?>
+        <?php if ($verses && $current_filter !== 'scripture') : ?>
             <div class="sermon-verses"><?php echo esc_html($verses); ?></div>
         <?php endif; ?>
-        <?php if ($sermon_date && !$show_date_scripture_in_one_row) : ?>
+        <?php if ($sermon_date && $current_filter !== 'recent') : ?>
             <div class="sermon_date"><?php echo esc_html($sermon_date); ?></div>
         <?php endif; ?>
-        <?php if ($show_date_scripture_in_one_row && ($sermon_date || $verses)) : ?>
-            <div class="sermon_date_verses"><?php echo esc_html($sermon_date . ' / ' . $verses); ?></div>
-        <?php endif; ?>
-        <?php if (!empty($speaker_names)) : ?>
+        <?php if (!empty($speaker_names) && $current_filter !== 'speaker') : ?>
             <div class="sermon-speaker">
                 <?php echo esc_html(implode(', ', $speaker_names)); ?>
             </div>
